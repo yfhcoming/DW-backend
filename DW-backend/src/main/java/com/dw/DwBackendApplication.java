56cf45976628cf2e4328da525147714c5b087900
Hylissang <1916297575@qq.com>
2021-12-19 19:17:05 +0800

    init

---
+++ DW-backend/src/main/java/com/dw/DwBackendApplication.java
@@ -0,0 +1,13 @@
+package com.dw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DwBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DwBackendApplication.class, args);
+    }
+
+}
---
+++ DW-backend/src/test/java/com/dw/DwBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DwBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
